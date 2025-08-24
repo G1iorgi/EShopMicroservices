e1cb5c5c475ada4747cf2dfa41244b810927eac8
G1iorgi <giorgiochxikidze121@gmail.com>
2025-08-24 19:06:42 +0400

    Initial

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using Basket.API.Models;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using FluentValidation;
+global using Mapster;
+global using Marten;
+global using MediatR;
+global using Basket.API.Basket.Exceptions;
+global using Basket.API.Data;
